3020dbedc8bf5082b4e1868459ac75266736be96
sunghogo <leinad5592@gmail.com>
2024-03-17 19:57:50 -0400

    S5: Control Flow and Exercises Finished

--- Exercises/Exercises/S5.cs
+++ Exercises/Exercises/S5.cs
@@ -98,5 +98,88 @@ namespace Exercises
                 Console.WriteLine($"{(demeritPoints <= 12 ? $"{demeritPoints} demerit points incurred" : "License suspended")}");
             }
         }
+
+        public static void Q5()
+        {
+            int count = 0;
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine($"Total numbers divisiable by 3 between 1 and 100: {count}");
+        }
+
+        public static void Q6()
+        {
+            int sum = 0;
+            do
+            {
+                Console.WriteLine("Please continuously enter integers, or enter 'ok' to exit and sum them up: ");
+                string? input = Console.ReadLine();
+                if (input == "ok")
+                {
+                    break;
+                }
+                int validatedInput = ValidateInput(input);
+                sum += validatedInput;
+            } while (true);
+            Console.WriteLine($"Total Sum: {sum}");
+        }
+
+        public static void Q7()
+        {
+            Console.WriteLine("Please enter an integer to compute its factorial: ");
+            string? input = Console.ReadLine();
+            int validatedInput = ValidateInput(input);
+
+            int factorial = 1;
+            for (int i = validatedInput; i > 0; i--)
+            {
+                factorial *= i;
+            }
+            Console.WriteLine($"{validatedInput}! = {factorial}");
+        }
+
+        public static void Q8()
+        {
+            Console.WriteLine("Guess the integer from 1 to 10: ");
+            Random random = new Random();
+            int randomNumber = random.Next(1, 10);
+
+            for (int tries = 4; tries > 0; tries--)
+            {
+                string? input = Console.ReadLine();
+                int guess = ValidateInput(input);
+                if (guess == randomNumber)
+                {
+                    Console.WriteLine("You won!");
+                    return;
+                } else
+                {
+                    Console.WriteLine($"Wrong! You have {tries-1} tries left.");
+                }
+            }
+            Console.WriteLine("You lost!");
+        }
+
+        public static void Q9()
+        {
+            Console.WriteLine("Please enter a series of integers separated by a comma: ");
+            string? input = Console.ReadLine();
+            
+            int[] numbers = Array.ConvertAll(input.Split(','), int.Parse);
+            int max = numbers[0];
+            foreach (int number in numbers)
+            {
+                if (number > max)
+                {
+                    max = number;
+                }
+            }
+            Console.WriteLine($"Maximum value: {max}");
+        }
     }
 }
