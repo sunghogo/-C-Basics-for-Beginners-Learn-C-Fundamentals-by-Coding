0af7a6fafe5c96a4203774b9acdb24af61daa39b
sunghogo <70732249+sunghogo@users.noreply.github.com>
2024-03-20 13:08:59 -0400

    S7: Working with Dates Finished

---
+++ DateTime/DateTime/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace DateTime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // DateTime objects are immutable
+            var dateTime = new System.DateTime(2023, 3, 20);
+
+            // DateTime properties
+            var now = System.DateTime.Now;
+            var today = System.DateTime.Today;
+
+            Console.WriteLine("Hour: " + now.Hour);
+            Console.WriteLine("Hour: " + now.Minute);
+
+            // DateTime methods to traverse exisiting objects
+            var tomorrow = now.AddDays(1);
+            var yesterday = now.AddDays(-1);
+
+            Console.WriteLine(tomorrow);
+            Console.WriteLine(yesterday);
+            Console.WriteLine(now);
+
+            // DateTime string formatting
+            Console.WriteLine(now.ToLongDateString());
+            Console.WriteLine(now.ToShortDateString());
+            Console.WriteLine(now.ToLongTimeString());
+            Console.WriteLine(now.ToShortTimeString());
+
+            Console.WriteLine(now.ToString("yy-MM-dd HH.mm"));
+        }
+    }
+}
---
+++ TimeSpan/TimeSpan/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace TimeSpan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating TimeSpan objects which are immutable
+            System.TimeSpan timeSpan = new System.TimeSpan(1, 2, 3);
+            System.TimeSpan timeSpan1 = new System.TimeSpan(1, 0, 0);
+
+            System.TimeSpan timeSpan2 = System.TimeSpan.FromHours(1);
+
+            // DateTime Subtraction
+            System.DateTime start = System.DateTime.Now;
+            System.DateTime end = System.DateTime.Now.AddMinutes(2);
+            System.TimeSpan duration = end - start;
+
+            Console.WriteLine($"Duration: {duration}");
+
+            // TimeSpan Properties
+            Console.WriteLine($"Minutes: {timeSpan.Minutes}");
+            Console.WriteLine($"Total Minutes: {timeSpan.TotalMinutes}");
+
+            // .Add() / .Subtract() Object Methods
+            Console.WriteLine($"Add Example: {timeSpan.Add(System.TimeSpan.FromMinutes(8))}");
+            Console.WriteLine($"Add Example: {timeSpan.Subtract(System.TimeSpan.FromMinutes(2))}");
+
+            // .ToString() Obj Method
+            Console.WriteLine($"ToString: {timeSpan.ToString()}");
+
+            // Parse() Static Method to return TimeSpan obj
+            Console.WriteLine($"Parse: {System.TimeSpan.Parse("01:02:03")}");
+        }
+    }
+}
